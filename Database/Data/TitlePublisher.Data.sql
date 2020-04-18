db72772c046e54ec1cc66b60f1f1b14e98c42a75
Evgeniy Baryshev <ebarishev@gmail.com>
2020-04-19 00:00:27 +0300

    892

--- Database/Data/TitlePublisher.Data.sql
+++ Database/Data/TitlePublisher.Data.sql
@@ -699,6 +699,8 @@ AS
 			, (18, 888, '', NULL)
 			, (5, 889, '', NULL)
 			, (2, 890, '5-03-001002-5', N'русск.'), (2, 890, '0-8176-3329-4', N'англ.')
+			, (5, 891, '5-02-013909-2', NULL)
+			, (2, 892, '', NULL)
 	) 
 	AS Source ([PublisherId], [TitleId], [PublisherOrder], [Isbn], [Remarks]) 
 	ON Target.[PublisherId] = Source.[PublisherId] AND Target.[TitleId] = Source.[TitleId] AND Target.[Isbn] = Source.[Isbn]
